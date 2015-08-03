42f043338e2d35cd20462e2a37e631d8133c46a8
EdKang <kange1@wit.edu>
2015-08-03 13:23:54 -0400

    Revert "NFC tag location"
    This reverts commit 605ac379a84e816ba855c9a8b3c1e1e9f655e899.

--- gen/edu/wit/apppath/R.java
+++ gen/edu/wit/apppath/R.java
@@ -9,54 +9,32 @@ package edu.wit.apppath;
 
 public final class R {
     public static final class array {
-        public static final int floor_array=0x7f060000;
-        public static final int room_arrays=0x7f060001;
+        public static final int floor_array=0x7f050000;
+        public static final int room_arrays=0x7f050001;
     }
     public static final class attr {
     }
-    public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
-         Example customization of dimensions originally defined in res/values/dimens.xml
-         (such as screen margins) for screens with more than 820dp of available width. This
-         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
-    
-         */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-    }
     public static final class drawable {
         public static final int direction=0x7f020000;
-        public static final int gif_first=0x7f020001;
-        public static final int gif_ground=0x7f020002;
-        public static final int gif_second=0x7f020003;
-        public static final int gif_third=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int main_building_floor0=0x7f020006;
-        public static final int main_building_floor1=0x7f020007;
-        public static final int main_building_floor2=0x7f020008;
-        public static final int main_building_floor3=0x7f020009;
-        public static final int map=0x7f02000a;
-        public static final int nfc=0x7f02000b;
-        public static final int png_first=0x7f02000c;
-        public static final int png_ground=0x7f02000d;
-        public static final int png_second=0x7f02000e;
-        public static final int png_third=0x7f02000f;
-        public static final int round_nfc=0x7f020010;
+        public static final int ic_launcher=0x7f020001;
+        public static final int main_building_floor0=0x7f020002;
+        public static final int main_building_floor1=0x7f020003;
+        public static final int main_building_floor2=0x7f020004;
+        public static final int main_building_floor3=0x7f020005;
+        public static final int map=0x7f020006;
+        public static final int nfc=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
-        public static final int button1=0x7f090004;
-        public static final int direction=0x7f090007;
-        public static final int floor_spinner=0x7f090005;
-        public static final int image=0x7f090009;
-        public static final int imageView1=0x7f09000a;
-        public static final int map=0x7f090006;
-        public static final int nfc=0x7f090008;
-        public static final int spinner1=0x7f090001;
-        public static final int spinner2=0x7f090003;
-        public static final int textView1=0x7f090000;
-        public static final int textView2=0x7f090002;
+        public static final int button1=0x7f070004;
+        public static final int direction=0x7f070007;
+        public static final int floor_spinner=0x7f070005;
+        public static final int image=0x7f070009;
+        public static final int map=0x7f070006;
+        public static final int nfc=0x7f070008;
+        public static final int spinner1=0x7f070001;
+        public static final int spinner2=0x7f070003;
+        public static final int textView1=0x7f070000;
+        public static final int textView2=0x7f070002;
     }
     public static final class layout {
         public static final int activity_directions=0x7f030000;
@@ -64,25 +42,20 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int activity_map=0x7f030003;
         public static final int activity_nfc=0x7f030004;
-        public static final int activity_nfclocation=0x7f030005;
-    }
-    public static final class menu {
-        public static final int nfclocation=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f05000a;
-        public static final int app_name=0x7f050000;
-        public static final int from=0x7f050007;
-        public static final int hello_world=0x7f050002;
-        public static final int room_prompt=0x7f05000b;
-        public static final int select=0x7f050009;
-        public static final int title_activity_directions=0x7f050004;
-        public static final int title_activity_floor_select=0x7f050006;
-        public static final int title_activity_main=0x7f050001;
-        public static final int title_activity_map=0x7f050003;
-        public static final int title_activity_nfc=0x7f050005;
-        public static final int title_activity_nfclocation=0x7f05000c;
-        public static final int to=0x7f050008;
+        public static final int action_settings=0x7f04000a;
+        public static final int app_name=0x7f040000;
+        public static final int from=0x7f040007;
+        public static final int hello_world=0x7f040002;
+        public static final int room_prompt=0x7f04000b;
+        public static final int select=0x7f040009;
+        public static final int title_activity_directions=0x7f040004;
+        public static final int title_activity_floor_select=0x7f040006;
+        public static final int title_activity_main=0x7f040001;
+        public static final int title_activity_map=0x7f040003;
+        public static final int title_activity_nfc=0x7f040005;
+        public static final int to=0x7f040008;
     }
     public static final class style {
         /** 
@@ -106,10 +79,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
