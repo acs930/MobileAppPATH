3f6a74584b4d046d64910d8ec6cf7c6689e5cf91
EdKang <kange1@wit.edu>
2015-08-03 13:23:42 -0400

    Revert "Add NFC details"
    This reverts commit 872246a7370fe73f3d0d1ba9e422ec345dd132e5.

--- gen/edu/wit/apppath/R.java
+++ gen/edu/wit/apppath/R.java
@@ -64,25 +64,12 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int activity_map=0x7f030003;
         public static final int activity_nfc=0x7f030004;
-        public static final int activity_nfcdobbs_first=0x7f030005;
-        public static final int activity_nfckingman=0x7f030006;
-        public static final int activity_nfclocation=0x7f030007;
-        public static final int activity_nfcrubenstain_first=0x7f030008;
-        public static final int activity_nfcwatson_first=0x7f030009;
-        public static final int activity_nfcwentworth_first=0x7f03000a;
-        public static final int activity_nfcwillson_first=0x7f03000b;
-        public static final int activity_nfcwillston_first=0x7f03000c;
+        public static final int activity_nfclocation=0x7f030005;
+        public static final int activity_nfcwentworth_first=0x7f030006;
     }
     public static final class menu {
-        public static final int nfcdobbs_first=0x7f080000;
-        public static final int nfckingman=0x7f080001;
-        public static final int nfclocation=0x7f080002;
-        public static final int nfcrubenstain=0x7f080003;
-        public static final int nfcrubenstain_first=0x7f080004;
-        public static final int nfcwatson_first=0x7f080005;
-        public static final int nfcwentworth_first=0x7f080006;
-        public static final int nfcwillson_first=0x7f080007;
-        public static final int nfcwillston_first=0x7f080008;
+        public static final int nfclocation=0x7f080000;
+        public static final int nfcwentworth_first=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f05000a;
@@ -96,15 +83,8 @@ public final class R {
         public static final int title_activity_main=0x7f050001;
         public static final int title_activity_map=0x7f050003;
         public static final int title_activity_nfc=0x7f050005;
-        public static final int title_activity_nfcdobbs_first=0x7f05000e;
-        public static final int title_activity_nfckingman=0x7f050014;
         public static final int title_activity_nfclocation=0x7f05000c;
-        public static final int title_activity_nfcrubenstain=0x7f05000f;
-        public static final int title_activity_nfcrubenstain_first=0x7f050010;
-        public static final int title_activity_nfcwatson_first=0x7f050011;
         public static final int title_activity_nfcwentworth_first=0x7f05000d;
-        public static final int title_activity_nfcwillson_first=0x7f050012;
-        public static final int title_activity_nfcwillston_first=0x7f050013;
         public static final int to=0x7f050008;
     }
     public static final class style {
