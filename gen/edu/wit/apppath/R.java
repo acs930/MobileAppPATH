66859278e2ae5b3ca403c25a8da37a632ee217ec
EdKang <kange1@wit.edu>
2015-08-05 19:30:54 -0400

    change scale

--- gen/edu/wit/apppath/R.java
+++ gen/edu/wit/apppath/R.java
@@ -42,7 +42,7 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f09000f;
-        public static final int button1=0x7f090003;
+        public static final int button1=0x7f090005;
         public static final int direction=0x7f090009;
         public static final int directionsList=0x7f090000;
         public static final int end_spinner=0x7f090006;
@@ -52,10 +52,10 @@ public final class R {
         public static final int map=0x7f090008;
         public static final int nfc=0x7f09000a;
         public static final int nfcButton=0x7f090001;
-        public static final int start_spinner=0x7f090005;
+        public static final int start_spinner=0x7f090004;
         public static final int string=0x7f09000e;
         public static final int textView1=0x7f090002;
-        public static final int textView2=0x7f090004;
+        public static final int textView2=0x7f090003;
         public static final int textView4=0x7f09000d;
     }
     public static final class layout {
